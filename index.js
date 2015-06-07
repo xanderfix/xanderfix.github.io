b956fdf0357391baaa95d14bf044c4c601123f1e
xanderfix <xanderfix@gmail.com>
2015-06-07 18:50:40 -0500

    New trivia and video pages

--- index.js
+++ index.js
@@ -2,35 +2,35 @@ var time = 0;
 function change() {
     setInterval(function change1() {
     if (time == 0) {
+         document.getElementById("WNdate").innerHTML = "6/4/2015";
+         document.getElementById("WNa").href = "oathToOrderCover.html";
+         document.getElementById("WNimg").src = "images/oathToOrderCover.png";
+         document.getElementById("WNp").innerHTML = "Oath to Order Cover<br>My cover of Oath to Order from<br><i>The Legend of Zelda: Majora's Mask</i>.";
+         document.getElementById("WN").style.width = "362px";
+    }
+    if (time == 1) {
          document.getElementById("WNdate").innerHTML = "5/28/2015";
          document.getElementById("WNa").href = "starFoxReview.html";
          document.getElementById("WNimg").src = "images/starFoxCorneria.jpeg";
          document.getElementById("WNp").innerHTML = "Star Fox Review<br>My review of the SNES space-shooter, <i>Star Fox</i>.";
          document.getElementById("WN").style.width = "361px";
     }
-    if (time == 1) {
+    if (time == 2) {
          document.getElementById("WNdate").innerHTML = "5/11/2015";
          document.getElementById("WNa").href = "legendOfZeldaStoryFillInTheBlanks.html";
          document.getElementById("WNimg").src = "images/tLoZStorySmall.jpg";
          document.getElementById("WNp").innerHTML = "Fill in the Blanks: <br>The Legend of Zelda Intro Story<br>A fill in the blanks game featuring the <br>story from <i>The Legend of Zelda</i>.";
          document.getElementById("WN").style.width = "281px"; 
     }
-    if (time == 2) {
-         document.getElementById("WNdate").innerHTML = "5/10/2015";
-         document.getElementById("WNa").href = "theLegendOfZeldaMusic.html";
-         document.getElementById("WNimg").src = "images/theLegendOfZeldaMusic.png";
-         document.getElementById("WNp").innerHTML = "The Legend of Zelda Music<br>All of the music from <i>The Legend of Zelda</i>.";
-         document.getElementById("WN").style.width = "318px";
-    }
     if (time == 3) {
-         document.getElementById("WNdate").innerHTML = "5/29/2015";
-         document.getElementById("WNa").href = "SNESlegendOfZeldaReferences.html";
-         document.getElementById("WNimg").src = "images/KSStriforceSmall.png";
-         document.getElementById("WNp").innerHTML = "SNES Legend of Zelda<br>Series References<br>All of the references to the Legend of Zelda series in SNES games.";
-         document.getElementById("WN").style.width = "318px";
+         document.getElementById("WNdate").innerHTML = "6/7/2015";
+         document.getElementById("WNa").href = "superMarioGalaxyReferences.html";
+         document.getElementById("WNimg").src = "images/pokeballPlanet.jpg";
+         document.getElementById("WNp").innerHTML = "Super Mario Galaxy References<br>All of the references to other Nintendo games in <i>Super Mario Galaxy</i>.";
+         document.getElementById("WN").style.width = "320px";
          time = -1;
     }
     time+=1;
-    }, 4000)
+    }, 4444)
 }
 
