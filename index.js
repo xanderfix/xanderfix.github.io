4afa828642a031fa28120b1b0b55a0af28af1a1d
xanderfix <xanderfix@gmail.com>
2015-06-15 15:26:59 -0500

    New trivia page

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var time = 0;
 function change() {
     setInterval(function change1() {
     if (time == 0) {
-         document.getElementById("WNdate").innerHTML = "6/7/2015";
-         document.getElementById("WNa").href = "superMarioGalaxyReferences.html";
-         document.getElementById("WNimg").src = "images/pokeballPlanet.jpg";
-         document.getElementById("WNp").innerHTML = "Super Mario Galaxy References<br>All of the references to other Nintendo games in <i>Super Mario Galaxy</i>.";
-         document.getElementById("WN").style.width = "320px";
+         document.getElementById("WNdate").innerHTML = "6/8/2015";
+         document.getElementById("WNa").href = "retroGamingMyHomeWithinMyHome.html";
+         document.getElementById("WNimg").src = "images/atari2600ArticleSize.jpeg";
+         document.getElementById("WNp").innerHTML = "Retro gaming: My home within my home <br>By Greg Betancourt Jr.<br>Greg Betancourt Jr. discusses why retro games are so amazing.";
+         document.getElementById("WN").style.width = "370px";
     }
     if (time == 1) {
          document.getElementById("WNdate").innerHTML = "6/4/2015";
@@ -23,11 +23,11 @@ function change() {
          document.getElementById("WN").style.width = "281px"; 
     }
     if (time == 3) {
-         document.getElementById("WNdate").innerHTML = "6/8/2015";
-         document.getElementById("WNa").href = "retroGamingMyHomeWithinMyHome.html";
-         document.getElementById("WNimg").src = "images/atari2600ArticleSize.jpeg";
-         document.getElementById("WNp").innerHTML = "Retro gaming: My home within my home <br>By Greg Betancourt Jr.<br>Greg Betancourt Jr. discusses why retro games are so amazing.";
-         document.getElementById("WN").style.width = "370px";
+         document.getElementById("WNdate").innerHTML = "6/15/2015";
+         document.getElementById("WNa").href = "windWakerTrivia.html";
+         document.getElementById("WNimg").src = "images/windWakerStatues.png";
+         document.getElementById("WNp").innerHTML = "Wind Waker Trivia<br>Trivia related to <i>The Legend of Zelda: Wind Waker</i>.";
+         document.getElementById("WN").style.width = "345px";
          time = -1;
     }
     time+=1;
