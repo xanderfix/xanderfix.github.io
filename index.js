a25473075a862f5968786621d98cf9ce917f8b1c
xanderfix <xanderfix@gmail.com>
2015-06-26 14:18:33 -0500

    New article

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ var time = 0;
 function change() {
     setInterval(function change1() {
     if (time == 0) {
+         document.getElementById("WNdate").innerHTML = "6/18/2015";
+         document.getElementById("WNa").href = "koopasThemeGuitarCover.html";
+         document.getElementById("WNimg").src = "images/koopasThemeGuitarCover.png";
+         document.getElementById("WNp").innerHTML = "Koopa's Theme Guitar Cover<br>My guitar cover of Koopa's Theme from <i>Super Mario 64</i>.";
+         document.getElementById("WN").style.width = "362px";
+    }
+    if (time == 1) {
          document.getElementById("WNdate").innerHTML = "6/15/2015";
          document.getElementById("WNa").href = "windWakerTrivia.html";
          document.getElementById("WNimg").src = "images/windWakerStatues.png";
          document.getElementById("WNp").innerHTML = "Wind Waker Trivia<br>Trivia related to <i>The Legend of Zelda: Wind Waker</i>.";
          document.getElementById("WN").style.width = "345px";
-    }
-    if (time == 1) {
-         document.getElementById("WNdate").innerHTML = "6/8/2015";
-         document.getElementById("WNa").href = "retroGamingMyHomeWithinMyHome.html";
-         document.getElementById("WNimg").src = "images/atari2600ArticleSize.jpeg";
-         document.getElementById("WNp").innerHTML = "Retro gaming: My home within my home <br>By Greg Betancourt Jr.<br>Greg Betancourt Jr. discusses why retro games are so amazing.";
-         document.getElementById("WN").style.width = "370px";
     }
     if (time == 2) {
          document.getElementById("WNdate").innerHTML = "5/11/2015";
@@ -23,11 +23,11 @@ function change() {
          document.getElementById("WN").style.width = "281px"; 
     }
     if (time == 3) {
-         document.getElementById("WNdate").innerHTML = "6/18/2015";
-         document.getElementById("WNa").href = "koopasThemeGuitarCover.html";
-         document.getElementById("WNimg").src = "images/koopasThemeGuitarCover.png";
-         document.getElementById("WNp").innerHTML = "Koopa's Theme Guitar Cover<br>My guitar cover of Koopa's Theme from <i>Super Mario 64</i>.";
-         document.getElementById("WN").style.width = "362px";
+         document.getElementById("WNdate").innerHTML = "6/26/2015";
+         document.getElementById("WNa").href = "5-coolest-things-from-super-mario-sunshine.html";
+         document.getElementById("WNimg").src = "images/sirenaBeachGamecubeControllerSmall.jpg";
+         document.getElementById("WNp").innerHTML = "5 Coolest Things from<br>Super Mario Sunshine<br>My list of the 5 coolest things <br>from <i>Super Mario Sunshine</i>.";
+         document.getElementById("WN").style.width = "320px";
          time = -1;
     }
     time+=1;
