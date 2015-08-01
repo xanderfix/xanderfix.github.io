66c8ada181d6e3c050ed624d395d40a9f5b73fe3
xanderfix <xanderfix@gmail.com>
2015-08-01 16:35:22 -0500

    Added new article.

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var time = 0;
 function change() {
     setInterval(function change1() {
     if (time == 0) {
-         document.getElementById("WNdate").innerHTML = "6/26/2015";
-         document.getElementById("WNa").href = "5-coolest-things-from-super-mario-sunshine.html";
-         document.getElementById("WNimg").src = "images/sirenaBeachGamecubeControllerSmall.jpg";
-         document.getElementById("WNp").innerHTML = "5 Coolest Things from<br>Super Mario Sunshine<br>My list of the 5 coolest things <br>from <i>Super Mario Sunshine</i>.";
-         document.getElementById("WN").style.width = "320px";
+         document.getElementById("WNdate").innerHTML = "7/2/2015";
+         document.getElementById("WNa").href = "5-coolest-things-from-super-mario-sunshine-video.html";
+         document.getElementById("WNimg").src = "images/5-coolest-things-from-super-mario-sunshine-video.png";
+         document.getElementById("WNp").innerHTML = "5 Coolest Things from<br>Super Mario Sunshine (Video)<br>My video of the 5 coolest things <br>from <i>Super Mario Sunshine</i>.";
+         document.getElementById("WN").style.width = "362px";
     }
     if (time == 1) {
          document.getElementById("WNdate").innerHTML = "6/15/2015";
@@ -18,15 +18,15 @@ function change() {
     if (time == 2) {
          document.getElementById("WNdate").innerHTML = "5/11/2015";
          document.getElementById("WNa").href = "legendOfZeldaStoryFillInTheBlanks.html";
-         document.getElementById("WNimg").src = "images/tLoZStorySmall.jpg";
+         document.getElementById("WNimg").src = "images/legendOFZeldaStoryFillInTheBlanks.jpg";
          document.getElementById("WNp").innerHTML = "Fill in the Blanks: <br>The Legend of Zelda Intro Story<br>A fill in the blanks game featuring the <br>story from <i>The Legend of Zelda</i>.";
          document.getElementById("WN").style.width = "281px"; 
     }
     if (time == 3) {
-         document.getElementById("WNdate").innerHTML = "7/2/2015";
-         document.getElementById("WNa").href = "5-coolest-things-from-super-mario-sunshine-video.html";
-         document.getElementById("WNimg").src = "images/5-coolest-things-from-super-mario-sunshine-video.png";
-         document.getElementById("WNp").innerHTML = "5 Coolest Things from<br>Super Mario Sunshine (Video)<br>My video of the 5 coolest things <br>from <i>Super Mario Sunshine</i>.";
+         document.getElementById("WNdate").innerHTML = "7/30/2015";
+         document.getElementById("WNa").href = "The-Mount-Rushmore-of-Video-Gaming.html";
+         document.getElementById("WNimg").src = "images/mountRushmoreOfGamingSmaller.jpg";
+         document.getElementById("WNp").innerHTML = 'The Mount Rushmore of Video Games<br>By Greg Betancourt Jr.<br> Greg discusses what video game characters he would carve onto a "Mount Rushmore of Video Gaming".';
          document.getElementById("WN").style.width = "362px";
          time = -1;
     }
