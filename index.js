20779c058ce29976a44ddc3b9f676f92f901ab26
xanderfix <xanderfix@gmail.com>
2015-08-04 20:49:35 -0500

    New trivia page.

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ var time = 0;
 function change() {
     setInterval(function change1() {
     if (time == 0) {
+         document.getElementById("WNdate").innerHTML = "7/30/2015";
+         document.getElementById("WNa").href = "The-Mount-Rushmore-of-Video-Gaming.html";
+         document.getElementById("WNimg").src = "images/mountRushmoreOfGamingSmaller.jpg";
+         document.getElementById("WNp").innerHTML = 'The Mount Rushmore of Video Games<br>By Greg Betancourt Jr.<br> Greg discusses what video game characters he would carve onto a "Mount Rushmore of Video Gaming".';
+         document.getElementById("WN").style.width = "362px";
+    }
+    if (time == 1) {
          document.getElementById("WNdate").innerHTML = "7/2/2015";
          document.getElementById("WNa").href = "5-coolest-things-from-super-mario-sunshine-video.html";
          document.getElementById("WNimg").src = "images/5-coolest-things-from-super-mario-sunshine-video.png";
          document.getElementById("WNp").innerHTML = "5 Coolest Things from<br>Super Mario Sunshine (Video)<br>My video of the 5 coolest things <br>from <i>Super Mario Sunshine</i>.";
          document.getElementById("WN").style.width = "362px";
-    }
-    if (time == 1) {
-         document.getElementById("WNdate").innerHTML = "6/15/2015";
-         document.getElementById("WNa").href = "windWakerTrivia.html";
-         document.getElementById("WNimg").src = "images/windWakerStatues.png";
-         document.getElementById("WNp").innerHTML = "Wind Waker Trivia<br>Trivia related to <i>The Legend of Zelda: Wind Waker</i>.";
-         document.getElementById("WN").style.width = "345px";
     }
     if (time == 2) {
          document.getElementById("WNdate").innerHTML = "5/11/2015";
@@ -23,11 +23,11 @@ function change() {
          document.getElementById("WN").style.width = "281px"; 
     }
     if (time == 3) {
-         document.getElementById("WNdate").innerHTML = "7/30/2015";
-         document.getElementById("WNa").href = "The-Mount-Rushmore-of-Video-Gaming.html";
-         document.getElementById("WNimg").src = "images/mountRushmoreOfGamingSmaller.jpg";
-         document.getElementById("WNp").innerHTML = 'The Mount Rushmore of Video Games<br>By Greg Betancourt Jr.<br> Greg discusses what video game characters he would carve onto a "Mount Rushmore of Video Gaming".';
-         document.getElementById("WN").style.width = "362px";
+         document.getElementById("WNdate").innerHTML = "8/4/2015";
+         document.getElementById("WNa").href = "Kirby-Level-Name-Acronyms.html";
+         document.getElementById("WNimg").src = "images/lollipopLand.png";
+         document.getElementById("WNp").innerHTML = "Kirby Level Name Acronyms<br>Investigating the secrets of Kirby series level names.";
+         document.getElementById("WN").style.width = "420px";
          time = -1;
     }
     time+=1;
