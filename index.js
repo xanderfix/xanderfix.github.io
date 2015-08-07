c04a1999147d11e00661df840e0962a10af0b706
xanderfix <xanderfix@gmail.com>
2015-08-07 16:07:48 -0500

    New game.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function change() {
          document.getElementById("WNdate").innerHTML = "7/30/2015";
          document.getElementById("WNa").href = "The-Mount-Rushmore-of-Video-Gaming.html";
          document.getElementById("WNimg").src = "images/mountRushmoreOfGamingSmaller.jpg";
-         document.getElementById("WNp").innerHTML = 'The Mount Rushmore of Video Games<br>By Greg Betancourt Jr.<br> Greg discusses what video game characters he would carve onto a "Mount Rushmore of Video Gaming".';
+         document.getElementById("WNp").innerHTML = 'The Mount Rushmore of Video Games<br>By Greg Betancourt Jr.<br><br> Greg discusses what video game characters he would carve onto a "Mount Rushmore of Video Gaming".';
          document.getElementById("WN").style.width = "362px";
     }
     if (time == 1) {
@@ -19,23 +19,22 @@ function change() {
          document.getElementById("WNdate").innerHTML = "7/2/2015";
          document.getElementById("WNa").href = "5-coolest-things-from-super-mario-sunshine-video.html";
          document.getElementById("WNimg").src = "images/5-coolest-things-from-super-mario-sunshine-video.png";
-         document.getElementById("WNp").innerHTML = "5 Coolest Things from<br>Super Mario Sunshine (Video)<br>My video of the 5 coolest things <br>from <i>Super Mario Sunshine</i>.";
+         document.getElementById("WNp").innerHTML = "5 Coolest Things from<br>Super Mario Sunshine (Video)<br><br>My video of the 5 coolest things from <i>Super Mario Sunshine</i>.";
          document.getElementById("WN").style.width = "362px";
     }
     if (time == 3) {
-         document.getElementById("WNdate").innerHTML = "5/11/2015";
-         document.getElementById("WNa").href = "legendOfZeldaStoryFillInTheBlanks.html";
-         document.getElementById("WNimg").src = "images/legendOFZeldaStoryFillInTheBlanks.jpg";
-         document.getElementById("WNp").innerHTML = "Fill in the Blanks: <br>The Legend of Zelda Intro Story<br>A fill in the blanks game featuring the <br>story from <i>The Legend of Zelda</i>.";
-         document.getElementById("WN").style.width = "281px"; 
-         time = -1;
-    }
-    if (time == 4) {
-         document.getElementById("WNdate").innerHTML = "8/4/2015";
+          document.getElementById("WNdate").innerHTML = "8/4/2015";
          document.getElementById("WNa").href = "Kirby-Level-Name-Acronyms.html";
          document.getElementById("WNimg").src = "images/lollipopLand.png";
-         document.getElementById("WNp").innerHTML = "Kirby Level Name Acronyms<br>Investigating the secrets of Kirby series level names.";
+         document.getElementById("WNp").innerHTML = "Kirby Level Name Acronyms<br><br>Investigating the secrets of Kirby series level names.";
          document.getElementById("WN").style.width = "420px";
+    }
+    if (time == 4) {
+         document.getElementById("WNdate").innerHTML = "8/7/2015";
+         document.getElementById("WNa").href = "Jigsaw-Puzzle-Bring-on-the-Super-Ability.html";
+         document.getElementById("WNimg").src = "images/superAbilitiesSmall.jpg";
+         document.getElementById("WNp").innerHTML = "Jigsaw Puzzle: <br>Bring on the Super Ability!<br><br>A jigsaw puzzle using the official artwork for Kirby's Super Abilities from <i>Kirby's Return to Dreamland</i>.";
+         document.getElementById("WN").style.width = "362px";
          time = -1;
     }
     time+=1;
