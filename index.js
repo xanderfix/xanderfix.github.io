27af79263d7a0bff003ade6fb08bc3ce7c30e5d6
xanderfix <xanderfix@gmail.com>
2015-08-08 13:12:16 -0500

    Fixed home page.

--- index.js
+++ index.js
@@ -2,32 +2,32 @@ var time = 0;
 function change() {
     setInterval(function change1() {
     if (time == 0) {
+         document.getElementById("WNdate").innerHTML = "8/4/2015";
+         document.getElementById("WNa").href = "Kirby-Level-Name-Acronyms.html";
+         document.getElementById("WNimg").src = "images/lollipopLand.png";
+         document.getElementById("WNp").innerHTML = "Kirby Level Name Acronyms<br><br>Investigating the secrets of Kirby series level names.";
+         document.getElementById("WN").style.width = "420px";
+    }
+    if (time == 1) {
          document.getElementById("WNdate").innerHTML = "7/30/2015";
          document.getElementById("WNa").href = "The-Mount-Rushmore-of-Video-Gaming.html";
          document.getElementById("WNimg").src = "images/mountRushmoreOfGamingSmaller.jpg";
          document.getElementById("WNp").innerHTML = 'The Mount Rushmore of Video Games<br>By Greg Betancourt Jr.<br><br> Greg discusses what video game characters he would carve onto a "Mount Rushmore of Video Gaming".';
          document.getElementById("WN").style.width = "362px";
     }
-    if (time == 1) {
+    if (time == 2) {
          document.getElementById("WNdate").innerHTML = "7/11/2015";
          document.getElementById("WNa").href = "songOfStormsRemix.html";
          document.getElementById("WNimg").src = "images/songOfStormsRemix.png";
          document.getElementById("WNp").innerHTML = "Song of Storms Remix (MIDI)";
          document.getElementById("WN").style.width = "320px";
     }
-    if (time == 2) {
+    if (time == 3) {
          document.getElementById("WNdate").innerHTML = "7/2/2015";
          document.getElementById("WNa").href = "5-coolest-things-from-super-mario-sunshine-video.html";
          document.getElementById("WNimg").src = "images/5-coolest-things-from-super-mario-sunshine-video.png";
          document.getElementById("WNp").innerHTML = "5 Coolest Things from<br>Super Mario Sunshine (Video)<br><br>My video of the 5 coolest things from <i>Super Mario Sunshine</i>.";
          document.getElementById("WN").style.width = "362px";
-    }
-    if (time == 3) {
-          document.getElementById("WNdate").innerHTML = "8/4/2015";
-         document.getElementById("WNa").href = "Kirby-Level-Name-Acronyms.html";
-         document.getElementById("WNimg").src = "images/lollipopLand.png";
-         document.getElementById("WNp").innerHTML = "Kirby Level Name Acronyms<br><br>Investigating the secrets of Kirby series level names.";
-         document.getElementById("WN").style.width = "420px";
     }
     if (time == 4) {
          document.getElementById("WNdate").innerHTML = "8/7/2015";
