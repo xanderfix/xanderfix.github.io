a295b830c2ee37ff0a553ac4bea8a8e853449057
xanderfix <xanderfix@gmail.com>
2015-10-13 19:42:49 -0500

    2 new videos and music pages.

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ var time = 0;
 function change() {
     setInterval(function change1() {
     if (time == 0) {
+         document.getElementById("WNdate").innerHTML = "10/9/2015";
+         document.getElementById("WNa").href = "How-to-Play-the-Ocarina-of-Time-Warp-Songs.html";
+         document.getElementById("WNimg").src = "images/Ocarina of Time Medallions.png";
+         document.getElementById("WNp").innerHTML = "How to Play the Ocarina of Time Warp Songs";
+    }
+    if (time == 1) {
          document.getElementById("WNdate").innerHTML = "9/25/2015";
          document.getElementById("WNa").href = "A-Link-to-the-Past-Trivia.html";
          document.getElementById("WNimg").src = "images/cucooLady.png";
          document.getElementById("WNp").innerHTML = "A Link to the Past Trivia<br><br> Cool A Link to the Past trivia; including a secret about the Cucoos.";
-    }
-    if (time == 1) {
-         document.getElementById("WNdate").innerHTML = "9/23/2015";
-         document.getElementById("WNa").href = "Zeldas-Rescue-Acoustic-Guitar-Cover.html";
-         document.getElementById("WNimg").src = "images/Zelda's Rescue Acoustic Guitar Cover.jpg";
-         document.getElementById("WNp").innerHTML = "Zelda's Rescue Acoustic Guitar Cover<br><br>My acoustic guitar cover of Zelda's Rescue from A Link to the Past."
     }
     if (time == 2) {
          document.getElementById("WNdate").innerHTML = "9/23/2015";
@@ -26,10 +26,10 @@ function change() {
          document.getElementById("WNp").innerHTML = "Jigsaw Puzzle: <br>Super Mario Bros. 3 World Maps<br><br>Jigsaw puzzles for the <i>Super Mario Bros. 3</i> world maps.";
     }
     if (time == 4) {
-         document.getElementById("WNdate").innerHTML = "9/28/2015";
-         document.getElementById("WNa").href = "How-to-Play-Zeldas-Rescue.html";
-         document.getElementById("WNimg").src = "images/Zelda's Rescue.jpg";
-         document.getElementById("WNp").innerHTML = "How to Play Zelda's Rescue";
+         document.getElementById("WNdate").innerHTML = "10/11/2015";
+         document.getElementById("WNa").href = "Ocarina-of-Time-Warp-Songs-Piano-Medley.html";
+         document.getElementById("WNimg").src = "images/Ocarina of Time Warp Songs Piano Medley.png";
+         document.getElementById("WNp").innerHTML = "Ocarina of Time Warp Songs Piano Medley<br><br>My piano medley of slightly remixed versions of the Ocarina of Time warp songs.";
          time = -1;
     }
     time+=1;
