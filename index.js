ea03d19f81fbcb9de1b923867006efda10b6ea54
xanderfix <xanderfix@gmail.com>
2015-10-25 17:15:07 -0500

    2 new music pages.

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var time = 0;
 function change() {
     setInterval(function change1() {
     if (time == 0) {
-         document.getElementById("WNdate").innerHTML = "10/16/2015";
-         document.getElementById("WNa").href = "How-to-Play-the-Heavy-Lobster-Theme.html";
-         document.getElementById("WNimg").src = "images/How to Play the Heavy Lobster Theme.png";
-         document.getElementById("WNp").innerHTML = "How to Play the Heavy Lobster Theme";
+         document.getElementById("WNdate").innerHTML = "10/24/2015";
+         document.getElementById("WNa").href = "A-Beginners-Guide-to-Splatoon-Part-3-Clothing-and-Weapons.html";
+         document.getElementById("WNimg").src = "images/ammoKnights.jpg";
+         document.getElementById("WNp").innerHTML = "Part 3 Clothing and Weapons<br><br> The third part of my beginner's guide to <i>Splatoon</i> explaining the functions and purchasing of clothing and weapons."
     }
     if (time == 1) {
          document.getElementById("WNdate").innerHTML = "10/15/2015";
@@ -26,10 +26,10 @@ function change() {
          document.getElementById("WNp").innerHTML = "Jigsaw Puzzle: <br>Super Mario Bros. 3 World Maps<br><br>Jigsaw puzzles for the <i>Super Mario Bros. 3</i> world maps.";
     }
     if (time == 4) {
-         document.getElementById("WNdate").innerHTML = "10/24/2015";
-         document.getElementById("WNa").href = "A-Beginners-Guide-to-Splatoon-Part-3-Clothing-and-Weapons.html";
-         document.getElementById("WNimg").src = "images/ammoKnights.jpg";
-         document.getElementById("WNp").innerHTML = "Part 3 Clothing and Weapons<br><br> The third part of my beginner's guide to <i>Splatoon</i> explaining the functions and purchasing of clothing and weapons."
+         document.getElementById("WNdate").innerHTML = "10/25/2015";
+         document.getElementById("WNa").href = "How-to-Play-Drawcia-Sorceress.html";
+         document.getElementById("WNimg").src = "images/DrawciaSorceress.png";
+         document.getElementById("WNp").innerHTML = "How to Play Drawcia Sorceress";
          time = -1;
     }
     time+=1;
