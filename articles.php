cfa44cf1940386fb03c665d0c008176f0146828f
xanderfix <xanderfix@gmail.com>
2015-10-28 11:56:33 -0500

    PHP test.

---
+++ articles.php
@@ -0,0 +1,115 @@
+<html>
+<head>
+<title>Articles | Zelda Rocks</title>
+<meta name="descripiton" content="Nintendo video game articles from NES to Wii U.">
+<link type="text/css" rel="stylesheet" href="allFiles.css"/>
+<link type="text/css" rel="stylesheet" href="articles.css"/>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-54960566-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</head>
+
+<body>
+<div id="navbar">
+        <ul>
+           <a href="index.html"><li><div class="link">Home</div></a></li>
+           <a href="articles.html"><li><div class="link">Articles</div></a></li>
+           <a href="trivia.html"><li><div class="link">Trivia</div></a></li>
+           <a href="videos.html"><li><div class="link">Videos</div></a></li>
+           <a href="music.html"><li><div class="link">Music</div></a></li>
+           <a href="polls.html"><li><div class="link">Polls</div></a></li>
+           <a href="games.html"><li><div class="link">Games</div></a></li>
+           <a href="contact.html"><li><div class="link">Contact</div></a></li>
+        </ul>
+    </div>
+<div id="left">
+    <a href="marioStuff.html"><img id="navImg1" class="leftimg"/></a>
+    <a href="zeldaStuff.html"><img id="navImg3" class="leftimg"/></a>
+    <a href="metroidStuff.html"><img id="navImg5" class="leftimg"/></a>
+    <a href="kirbyStuff.html"><img id="navImg7" class="leftimg"/></a>
+    <a href="starFoxStuff.html"><img id="navImg9" class="leftimg"/></a>
+    <a href="donkeyKongStuff.html"><img id="navImg11" class="leftimg"/></a>
+    <a href="yoshiStuff.html"><img id="navImg13" class="leftimg"></a>
+    <a href="banjoKazooieStuff.html"><img id="navImg15" class="leftimg"/></a>
+    <a href="superSmashBrosStuff.html"><img id="navImg17" class="leftimg"/></a>
+    <a href="pikminStuff.html"><img id="navImg19" class="leftimg"/></a>
+    <a href="animalCrossingStuff.html"><img id="navImg21" class="leftimg"/></a>
+    <a href="splatoonStuff.html"><img id="navImg23" class="leftimg"/></a>
+</div>
+<div id="right">
+    <a href="marioStuff.html"><img id="navImg2" class="rightimg"/></a>
+    <a href="zeldaStuff.html"><img id="navImg4" class="rightimg"/></a>
+    <a href="metroidStuff.html"><img id="navImg6" class="rightimg"/></a>
+    <a href="kirbyStuff.html"><img id="navImg8" class="rightimg"/></a>
+    <a href="starFoxStuff.html"><img id="navImg10" class="leftimg"/></a>
+    <a href="donkeyKongStuff.html"><img id="navImg12" class="rightimg"/></a>
+    <a href="yoshiStuff.html"><img id="navImg14" class="leftimg"></a>
+    <a href="banjoKazooieStuff.html"><img id="navImg16" class="rightimg"/></a>
+    <a href="superSmashBrosStuff.html"><img id="navImg18" class="rightimg"/></a>
+    <a href="pikminStuff.html"><img id="navImg20" class="rightimg"/></a>
+    <a href="animalCrossingStuff.html"><img id="navImg22" class="leftimg"/></a>
+    <a href="splatoonStuff.html"><img id="navImg24" class="leftimg"/></a>
+</div>
+<div id="main">
+    <h1 id="title">Articles</h1>
+    <a href="A-Beginners-Guide-to-Splatoon-Part-3-Clothing-and-Weapons.html">
+    <div class="article">
+        <div class="articlepdiv2">
+            <p class="articlep">A Beginner's Guide to Splatoon:<br> Part 3 Clothing and Weapons<br><br> The third part of my beginner's guide to <i>Splatoon</i> explaining the functions and purchasing of clothing and weapons.</p>
+        </div>
+        <div class="articleimgdiv">
+            <img class="articleimg2" src="images/ammoKnights.jpg"/>
+        </div>
+    </div>
+    </a>
+    <a href="A-Beginners-Guide-to-Splatoon-Part-2-Turf-Wars.html">
+    <div class="article">
+        <div class="articlepdiv2">
+            <p class="articlep">A Beginner's Guide to Splatoon:<br> Part 2 Turf Wars<br><br> The second part of my beginner's guide to <i>Splatoon</i> giving some handy tips for the game's main mode, Turf Wars.</p>
+        </div>
+        <div class="articleimgdiv">
+            <img class="articleimg2" src="images/splatoonBattle.jpg"/>
+        </div>
+    </div>
+    </a>
+    <a href="A-Beginners-Guide-to-Splatoon-Part-1-Inkopolis-Plaza.html">
+    <div class="article">
+        <div class="articlepdiv2">
+            <p class="articlep">A Beginner's Guide to Splatoon:<br> Part 1 Inkopolis Plaza<br><br> The first part of my beginner's guide to <i>Splatoon</i> covering  the game's hub area, Inkopolis Plaza.</p>
+        </div>
+        <div class="articleimgdiv">
+            <img class="articleimg2" src="images/inkopolisMap.jpg"/>
+        </div>
+    </div>
+    </a>
+    <a href="1-Year-Anniversary.html">
+    <div class="article">
+        <div class="articlepdiv">
+            <p class="articlep">1 Year Anniversary!<br><br> Celebrating the 1 year anniversary of this website and the 30th anniversary of Super Mario Bros.!</p>
+        </div>
+        <div class="articleimgdiv">
+            <img class="articleimg" src="images/superMarioBrosAndDuckHunt.jpg"/>
+        </div>
+    </div>
+    </a>
+    <a href="A-Brief-History-Of-Super-Mario-Bros-2.html">
+    <div class="article">
+        <div class="articlepdiv">
+            <p class="articlep">A Brief History Of: Super Mario Bros. 2<br><br> Exploring the strange history of <i>Super Mario Bros. 2</i>; which wasn't even originally a Mario game!</p>
+        </div>
+        <div class="articleimgdiv">
+            <img class="articleimg" src="images/superMarioBros2screenshot.jpg"/>
+        </div>
+    </div>
+    </a>
+    <pre class="nextPages"><a class="nextpage" href="articles.html">1</a>  <a class="nextPage" href="articles2.html">2</a>  <a class="nextPage" href="articles3.html">3</a>  <a class="nextPage" href="articles4.html">4</a></pre>
+</div>
+</body>
+</html>
