61e0f212d250d4166eeee33a6c52d3fbb254b416
xanderfix <xanderfix@gmail.com>
2016-02-25 12:52:23 -0600

    New music page for the Storm Eagle stage theme.

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ var time = 0;
 function change() {
     setInterval(function change1() {
     if (time == 0) {
+         document.getElementById("WNdate").innerHTML = "2/21/2015";
+         document.getElementById("WNa").href = "Farewell-Hyrule-King-Piano-Cover.html";
+         document.getElementById("WNimg").src = "images/FarewellHyruleKingPianoCover.png";
+         document.getElementById("WNp").innerHTML = "Farewell Hyrule King Piano Cover<br><br> My piano cover of Farewell Hyrule King from the ending of Wind Waker.";
+    }
+    if (time == 1) {
          document.getElementById("WNdate").innerHTML = "2/13/2015";
          document.getElementById("WNa").href = "Emulators-Amazing-Tools-or-Piracy-Devices.html";
          document.getElementById("WNimg").src = "images/phoneEmulator.jpg";
          document.getElementById("WNp").innerHTML = "Emulators: Amazing Tools or Piracy Devices?<br><br>A look at the types and uses of emulators; and why there is so much controversy over their use.";
-    }
-    if (time == 1) {
-         document.getElementById("WNdate").innerHTML = "1/18/2015";
-         document.getElementById("WNa").href = "How-to-Play-Green-Hill-Zone.html";
-         document.getElementById("WNimg").src = "images/greenHillZone.png";
-         document.getElementById("WNp").innerHTML = "How to Play Green Hill Zone";
     }
     if (time == 2) {
          document.getElementById("WNdate").innerHTML = "11/27/2015";
@@ -26,10 +26,10 @@ function change() {
          document.getElementById("WNp").innerHTML = "Jigsaw Puzzle: <br>Super Mario Bros. 3 World Maps<br><br>Jigsaw puzzles for the <i>Super Mario Bros. 3</i> world maps.";
     }
     if (time == 4) {
-         document.getElementById("WNdate").innerHTML = "2/21/2015";
-         document.getElementById("WNa").href = "Farewell-Hyrule-King-Piano-Cover.html";
-         document.getElementById("WNimg").src = "images/FarewellHyruleKingPianoCover.png";
-         document.getElementById("WNp").innerHTML = "Farewell Hyrule King Piano Cover<br><br> My piano cover of Farewell Hyrule King from the ending of Wind Waker.";
+         document.getElementById("WNdate").innerHTML = "2/25/2015";
+         document.getElementById("WNa").href = "How-to-Play-the-Storm-Eagle-Stage-Theme.html";
+         document.getElementById("WNimg").src = "images/stormEagle.jpg";
+         document.getElementById("WNp").innerHTML = "How to Play the Storm Eagle Stage Theme";
          time = -1;
     }
     time+=1;
