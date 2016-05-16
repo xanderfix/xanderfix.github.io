b77e9605ddabd8b21a45965f997b6b303c09b981
xanderfix <xanderfix@gmail.com>
2016-05-16 12:54:51 -0500

    New music page.

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ var time = 0;
 function change() {
     setInterval(function change1() {
     if (time == 0) {
+         document.getElementById("WNdate").innerHTML = "5/16/2016";
+         document.getElementById("WNa").href = "How-to-Play-the-Yoshis-Island-Story-Theme.html";
+         document.getElementById("WNimg").src = "images/yoshisIslandStoryTheme.png";
+         document.getElementById("WNp").innerHTML = "How to Play the Yoshi's Island Story Theme";
+    }
+    if (time == 1) {
          document.getElementById("WNdate").innerHTML = "5/11/2016";
          document.getElementById("WNa").href = "Yoshis-Island-Story-Theme-Cover.html";
          document.getElementById("WNimg").src = "images/yoshisIslandStoryThemeCover.png";
          document.getElementById("WNp").innerHTML = "Yoshi's Island Story Theme Cover<br><br> My cover of the story theme from Super Mario World 2: Yoshi's Island.";
     }
-    if (time == 1) {
+    if (time == 2) {
          document.getElementById("WNdate").innerHTML = "4/13/2016";
          document.getElementById("WNa").href = "Interview-DKC-speedrunner-Antilles58.html";
          document.getElementById("WNimg").src = "images/antilles58DKC2.png";
          document.getElementById("WNp").innerHTML = "Interview:<br>Donkey Kong Country Speedrunner Antilles58<br><br>An interview with Antilles58, an expert Donkey Kong Country series speedrunner.";
-    }
-    if (time == 2) {
-         document.getElementById("WNdate").innerHTML = "4/4/2016";
-         document.getElementById("WNa").href = "How-to-Play-Dark-Minds-Theme.html";
-         document.getElementById("WNimg").src = "images/darkMind.png";
-         document.getElementById("WNp").innerHTML = "How to Play Dark Mind's Theme<br>(Dark Mind's Second Form)";
     }
     if (time == 3) {
          document.getElementById("WNdate").innerHTML = "3/6/2016";
