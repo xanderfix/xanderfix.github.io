3e87abe3c1c62cd70abdfef7a131f44ac1af97c7
xanderfix <xanderfix@gmail.com>
2016-06-23 12:40:42 -0600

    New video.

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-var time = 0;
+var time = 1;
 function change() {
     setInterval(function change1() {
     if (time == 0) {
+         document.getElementById("WNdate").innerHTML = "6/22/2016";
+         document.getElementById("WNa").href = "The-Legend-of-Zelda-Medley.html";
+         document.getElementById("WNimg").src = "images/GBApiano.png";
+         document.getElementById("WNp").innerHTML = "The Legend of Zelda Medley<br><br> An amazing Zelda medley by Nick, the newest member of ZeldaRocks!<br>(In case you can't tell Xander is writing this).";
+    }
+    if (time == 1) {
          document.getElementById("WNdate").innerHTML = "6/1/2016";
          document.getElementById("WNa").href = "How-to-Play-The-Greatest-Warrior-in-the-Galaxy.html";
          document.getElementById("WNimg").src = "images/galactaKnight.png";
          document.getElementById("WNp").innerHTML = "How to Play The Greatest Warrior in the Galaxy<br>(Galacta Knight's Theme)";
     }
-    if (time == 1) {
-         document.getElementById("WNdate").innerHTML = "5/11/2016";
-         document.getElementById("WNa").href = "Yoshis-Island-Story-Theme-Cover.html";
-         document.getElementById("WNimg").src = "images/yoshisIslandStoryThemeCover.png";
-         document.getElementById("WNp").innerHTML = "Yoshi's Island Story Theme Cover<br><br> My cover of the story theme from Super Mario World 2: Yoshi's Island.";
-    }
     if (time == 2) {
          document.getElementById("WNdate").innerHTML = "4/13/2016";
-         document.getElementById("WNa").href = "Interview-DKC-speedrunner-Antilles58.html";
-         document.getElementById("WNimg").src = "images/antilles58DKC2.png";
+         document.getElementById("WNa").href = "Interview-DKC-Speedrunner-Antilles58.html";
+         document.getElementById("WNimg").src = "images/Antilles58DKC2.png";
          document.getElementById("WNp").innerHTML = "Interview:<br>Donkey Kong Country Speedrunner Antilles58<br><br>An interview with Antilles58, an expert Donkey Kong Country series speedrunner.";
-    }
+    }     
     if (time == 3) {
          document.getElementById("WNdate").innerHTML = "3/6/2016";
          document.getElementById("WNa").href = "Jigsaw-Puzzles-Pokemon-20th-Mythicals-and-Mega-Evolutions.html";
