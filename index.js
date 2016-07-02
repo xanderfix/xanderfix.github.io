bff718a21f4c6dd5b88e9b36c1e1355d67af38d5
xanderfix <xanderfix@gmail.com>
2016-07-01 18:32:59 -0600

    Added latest video to the site.

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ var time = 1;
 function change() {
     setInterval(function change1() {
     if (time == 0) {
+         document.getElementById("WNdate").innerHTML = "7/1/2016";
+         document.getElementById("WNa").href = "Hyrule-Market-Theme-Cover.html";
+         document.getElementById("WNimg").src = "images/HyruleMarketThemeCover.png";
+         document.getElementById("WNp").innerHTML = "Hyrule Market Theme Cover<br><br>A new cover by Xander of the Hyrule Market Theme from Ocarina of Time performed on piano and acoustic guitar.";
+    }
+    if (time == 1) {
          document.getElementById("WNdate").innerHTML = "6/27/2016";
          document.getElementById("WNa").href = "How-to-Play-CROWNED.html";
          document.getElementById("WNimg").src = "images/magolorSoul.png";
          document.getElementById("WNp").innerHTML = "How to Play CROWNED";
     }
-    if (time == 1) {
-        document.getElementById("WNdate").innerHTML = "6/24/2016";
+    if (time == 2) {
+         document.getElementById("WNdate").innerHTML = "6/24/2016";
          document.getElementById("WNa").href = "The-Penny-Arcade-in-Manitou-Springs.html";
          document.getElementById("WNimg").src = "images/PennyArcade3.png";
          document.getElementById("WNp").innerHTML = "The Penny Arcade<br>in Manitou Springs<br><br>One of the oldest arcades in the west, with everything from Frogger to skeeball to penny machines!";
-    }
-    if (time == 2) {
-         document.getElementById("WNdate").innerHTML = "6/22/2016";
-         document.getElementById("WNa").href = "The-Legend-of-Zelda-Medley.html";
-         document.getElementById("WNimg").src = "images/GBApiano.png";
-         document.getElementById("WNp").innerHTML = "The Legend of Zelda Medley<br><br> An amazing Zelda medley by Nick, the newest member of ZeldaRocks!<br>(In case you can't tell Xander is writing this).";
     }     
     if (time == 3) {
          document.getElementById("WNdate").innerHTML = "3/6/2016";
