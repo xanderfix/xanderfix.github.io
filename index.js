d332984d0bedf882fbb8a4e2e8ab0002620bdd5f
xanderfix <xanderfix@gmail.com>
2016-07-26 14:12:20 -0600

    New article.

--- index.js
+++ index.js
@@ -2,28 +2,28 @@ var time = 1;
 function change() {
     setInterval(function change1() {
     if (time == 0) {
+         document.getElementById("WNdate").innerHTML = "7/26/2016";
+         document.getElementById("WNa").href = "Why-Nintendo-Vans-Are-Awesome.html";
+         document.getElementById("WNimg").src = "images/nintendoVans2.jpg";
+         document.getElementById("WNp").innerHTML = "Xander explains why Nintendo Vans are awesome and you should definitely get some.";
+    }
+    if (time == 1) {
          document.getElementById("WNdate").innerHTML = "7/10/2016";
          document.getElementById("WNa").href = "Eight-Melodies-Earthbound-Beginnings-Cover.html";
          document.getElementById("WNimg").src = "images/eightMelodiesCover.png";
          document.getElementById("WNp").innerHTML = "Eight Melodies (Earthbound Beginnings/Mother) Cover<br><br>Xander's cover of the Eight Melodies from Earthbound Beginnings/Mother performed on piano and acoustic guitar; plus a metal Mt. Itoi cover at the end.";
     }
-    if (time == 1) {
+    if (time == 2) {
          document.getElementById("WNdate").innerHTML = "7/2/2016";
          document.getElementById("WNa").href = "art.html";
          document.getElementById("WNimg").src = "images/smashMural3.jpg";
          document.getElementById("WNp").innerHTML = "Super Smash Bros. Mural";
-    }
-    if (time == 2) {
+    }     
+    if (time == 3) {
          document.getElementById("WNdate").innerHTML = "6/27/2016";
          document.getElementById("WNa").href = "How-to-Play-CROWNED.html";
          document.getElementById("WNimg").src = "images/magolorSoul.png";
          document.getElementById("WNp").innerHTML = "How to Play CROWNED";
-    }     
-    if (time == 3) {
-         document.getElementById("WNdate").innerHTML = "6/24/2016";
-         document.getElementById("WNa").href = "The-Penny-Arcade-in-Manitou-Springs.html";
-         document.getElementById("WNimg").src = "images/PennyArcade3.png";
-         document.getElementById("WNp").innerHTML = "The Penny Arcade<br>in Manitou Springs<br><br>One of the oldest arcades in the west, with everything from Frogger to skeeball to penny machines!";
     }
     if (time == 4) {
          document.getElementById("WNdate").innerHTML = "3/6/2016";
