09b544728941fd6ee921c97cf33d7afe26bb6450
xanderfix <xanderfix@gmail.com>
2016-08-18 19:18:32 -0600

    New cover and MIDIs.

--- index.js
+++ index.js
@@ -2,28 +2,28 @@ var time = 1;
 function change() {
     setInterval(function change1() {
     if (time == 0) {
+         document.getElementById("WNdate").innerHTML = "8/17/2016";
+         document.getElementById("WNa").href = "How-to-Play-The-Mega-Man-2-Soundtrack.html";
+         document.getElementById("WNimg").src = "images/megaMan2Soundtrack.jpg";
+         document.getElementById("WNp").innerHTML = "How to Play The Mega Man 2 Soundtrack (Work in Progress)";
+    }
+    if (time == 1) {
+         document.getElementById("WNdate").innerHTML = "8/11/2016";
+         document.getElementById("WNa").href = "Crocodile-Cacophony-Cover.html";
+         document.getElementById("WNimg").src = "images/crocodileCacophonyCover.png";
+         document.getElementById("WNp").innerHTML = "Crocodile Cacophony Cover<br><br>Xander's cover of Crocodile Cacophony (Kaptain K. Rool's Theme) from Donkey Kong Country 2 performed on guitar and keyboard, plus a Flight of the Zinger short at the end!";
+    }
+    if (time == 2) {
          document.getElementById("WNdate").innerHTML = "8/5/2016";
          document.getElementById("WNa").href = "Interview-Someone-Who-Actually-Caught-Em-All.html";
          document.getElementById("WNimg").src = "images/completePokedex.png";
          document.getElementById("WNp").innerHTML = "Interview:<br>Someone Who Actually Caught 'Em All<br><br>An interview with someone who completed the amazing feat of catching all 721 Pokemon!";
-    }
-    if (time == 1) {
+    }     
+    if (time == 3) {
          document.getElementById("WNdate").innerHTML = "7/27/2016";
          document.getElementById("WNa").href = "art.html";
          document.getElementById("WNimg").src = "images/Venusorcerer.png";
          document.getElementById("WNp").innerHTML = "The Venusorcerer<br><br>A chalk drawing by Xander and Chloe based on Nick's love of Venusaur's and wordplay (everyone's in on this one!).";
-    }
-    if (time == 2) {
-         document.getElementById("WNdate").innerHTML = "7/26/2016";
-         document.getElementById("WNa").href = "Home-Undertale-Ness-House-Cover.html";
-         document.getElementById("WNimg").src = "images/homeUndertaleCover.png";
-         document.getElementById("WNp").innerHTML = "Home (Undertale)/Ness's House Cover<br><br>Xander and Nick's duet arrangement of Home from Undertale and Ness's House (Home Sweet Home) from Earthbound.";
-    }     
-    if (time == 3) {
-         document.getElementById("WNdate").innerHTML = "6/27/2016";
-         document.getElementById("WNa").href = "How-to-Play-CROWNED.html";
-         document.getElementById("WNimg").src = "images/magolorSoul.png";
-         document.getElementById("WNp").innerHTML = "How to Play CROWNED";
     }
     if (time == 4) {
          document.getElementById("WNdate").innerHTML = "3/6/2016";
