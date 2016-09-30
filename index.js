9ee1eb59a400c2c9cb12089dbbdb7155c793cb98
xanderfix <xanderfix@gmail.com>
2016-09-29 18:13:40 -0600

    New MIDI.

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ var time = 1;
 function change() {
     setInterval(function change1() {
     if (time == 0) {
+         document.getElementById("WNdate").innerHTML = "9/24/2016";
+         document.getElementById("WNa").href = "How-to-Play-Hyper-Zone-2.html";
+         document.getElementById("WNimg").src = "images/KDL3Zero.png";
+         document.getElementById("WNp").innerHTML = "How to Play Hyper Zone 2<br>(Vs. Zero KDL3)";
+    }
+    if (time == 1) {
          document.getElementById("WNdate").innerHTML = "9/13/2016";
          document.getElementById("WNa").href = "2-Year-Anniversary.html";
          document.getElementById("WNimg").src = "images/Nicole.gif";
          document.getElementById("WNp").innerHTML = "2 Year Anniversary!<br><br>Celebrating the second anniversary of ZeldaRocks plus a preview of a cool new music project.";
     }
-    if (time == 1) {
+    if (time == 2) {
          document.getElementById("WNdate").innerHTML = "8/30/2016";
          document.getElementById("WNa").href = "Gravity-Beetle-Cover.html";
          document.getElementById("WNimg").src = "images/gravityBeetleCover.png";
          document.getElementById("WNp").innerHTML = "Gravity Beetle Cover<br><br>Xander's cover of Gravity Beetle from Mega Man X3, plus a cover of X-Hunter stage 1 from X2 at the end (featuring Xander's dogs).";
-    }
-    if (time == 2) {
-         document.getElementById("WNdate").innerHTML = "8/27/2016";
-         document.getElementById("WNa").href = "How-to-Play-Real-Dark-Matter.html";
-         document.getElementById("WNimg").src = "images/darkMatter.png";
-         document.getElementById("WNp").innerHTML = "How to Play Real Dark Matter<br>(Kirby's Dream Land 2 Final Boss)";
     }     
     if (time == 3) {
          document.getElementById("WNdate").innerHTML = "7/27/2016";
