e542149b4fb04ad8a4b1cc1209c74e37f36e467c
Xander <xanderfix@gmail.com>
2017-03-18 23:06:42 -0600

    Nicole's first article.

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ var time = 1;
 function change() {
     setInterval(function change1() {
     if (time == 0) {
-         document.getElementById("WNdate").innerHTML = "1/28/2016";
-         document.getElementById("WNa").href = "The-Legacy-Final-Fantasy-1-Tribute-Album.html";
-         document.getElementById("WNimg").src = "images/TheLegacyAlbumCoverSmall.png";
-         document.getElementById("WNp").innerHTML = "The Legacy: Final Fantasy 1 Tribute Album<br><br>An amazing Final Fantasy 1 tribute album by Pixel Mixers. The best part? It's now available to download for free!";
+         document.getElementById("WNdate").innerHTML = "3/18/2016";
+         document.getElementById("WNa").href = "Revisiting-Pokemon-Red-Rescue-Team-The-Adventures-of-Team-LANmolas.html";
+         document.getElementById("WNimg").src = "images/PokemonRedRescueTeamBox.jpg";
+         document.getElementById("WNp").innerHTML = "Revisiting Pokemon Red Rescue Team: The Adventures of Team LANmolas<br><br>Nicole revisits her favorite childhood game, Pokemon Red Rescue Team, and comically finds it wasn't anywhere close to as good as she remembered.";
     }
     if (time == 1) {
-         document.getElementById("WNdate").innerHTML = "1/27/2016";
-         document.getElementById("WNa").href = "https://www.youtube.com/watch?v=Y4kM1eyIM-U&t=4s";
-         document.getElementById("WNimg").src = "images/ALttPPrologue.png";
-         document.getElementById("WNp").innerHTML = "A Link to the Past - Prologue Cover<br><br>Xander's cover of the classic prologue theme from The Legend of Zelda: A Link to the Past.";
+         document.getElementById("WNdate").innerHTML = "2/24/2016";
+         document.getElementById("WNa").href = "https://www.youtube.com/watch?v=wAdqNVmy3Ks";
+         document.getElementById("WNimg").src = "images/FinalFantasy4BossCover.png";
+         document.getElementById("WNp").innerHTML = "Final Fantasy 4 - Boss + Zeromus Cover<br><br>Xander's dual cover of the standard and final boss themes from Final Fantasy 4, accompanied by a sweet (though slightly cheesy) RPG style battle video!";
     }
     if (time == 2) {
          document.getElementById("WNdate").innerHTML = "12/24/2016";
@@ -23,7 +23,7 @@ function change() {
          document.getElementById("WNdate").innerHTML = "7/27/2016";
          document.getElementById("WNa").href = "art.html";
          document.getElementById("WNimg").src = "images/Venusorcerer.png";
-         document.getElementById("WNp").innerHTML = "The Venusorcerer<br><br>A chalk drawing by Xander and Chloe based on Nick's love of Venusaur's and wordplay (everyone's in on this one!).";
+         document.getElementById("WNp").innerHTML = "The Venusorcerer<br><br>A chalk drawing by Xander and Chloe based on Nicole's love of Venusaur's and wordplay (everyone's in on this one!).";
     }
     if (time == 4) {
          document.getElementById("WNdate").innerHTML = "3/6/2016";
