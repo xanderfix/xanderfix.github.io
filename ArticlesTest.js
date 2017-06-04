b21c07b2804f39894c626ac2e3d0ef77145bc9dc
Xander <xanderfix@gmail.com>
2017-06-03 22:26:47 -0600

    Test #6

--- ArticlesTest.js
+++ ArticlesTest.js
@@ -1,43 +1,7 @@
 function change() {
-    document.getElementById("title").innerHTRML = "More Than Just a Title Now!";
-    /*if (document.getElementById("Test").innerHTML == "True") {
+    document.getElementById("title").innerHTML = "More Than Just a Title Now!";
+    if (document.getElementById("Test").innerHTML == "True") {
          document.getElementById("Test").innerHTML = "It actually worked! (In case anyone else reads this, it originally said True before you even saw the page finish loading.)";
     }
-    if (time == 0) {
-         document.getElementById("WNdate").innerHTML = "6/1/2017";
-         document.getElementById("WNa").href = "Zelda-II-Boss-Battle-Cover.html";
-         document.getElementById("WNimg").src = "images/ZeldaIIBossBattleCover.png";
-         document.getElementById("WNp").innerHTML = "Zelda II - Boss Battle Cover<br><br>Xander's synth cover of Boss Battle from Zelda II: The Adventure of Link for the new Zelda tribute album "Hylian Downfall".";
-    }
-    if (time == 1) {
-         document.getElementById("WNdate").innerHTML = "3/18/2017";
-         document.getElementById("WNa").href = "Revisiting-Pokemon-Red-Rescue-Team-The-Adventures-of-Team-LANmolas.html";
-         document.getElementById("WNimg").src = "images/PokemonRedRescueTeamBox.jpg";
-         document.getElementById("WNp").innerHTML = "Revisiting Pokemon Red Rescue Team: The Adventures of Team LANmolas<br><br>Nicole revisits her favorite childhood game, Pokemon Red Rescue Team, and comically finds it wasn't anywhere close to as good as she remembered.";
-    }
-    if (time == 2) {
-         document.getElementById("WNdate").innerHTML = "12/24/2016";
-         document.getElementById("WNa").href = "How-to-Play-Jangle-Bells.html";
-         document.getElementById("WNimg").src = "images/DKC3merry.png";
-         document.getElementById("WNp").innerHTML = "How to Play Jangle Bells<br>(DKC3 MERRY Cheat)";
-    }     
-    if (time == 3) {
-         document.getElementById("WNdate").innerHTML = "7/27/2016";
-         document.getElementById("WNa").href = "art.html";
-         document.getElementById("WNimg").src = "images/Venusorcerer.png";
-         document.getElementById("WNp").innerHTML = "The Venusorcerer<br><br>A chalk drawing by Xander and Chloe based on Nicole's love of Venusaur's and wordplay (everyone's in on this one!).";
-    }
-    if (time == 4) {
-         document.getElementById("WNdate").innerHTML = "3/6/2016";
-         document.getElementById("WNa").href = "Jigsaw-Puzzles-Pokemon-20th-Mythicals-and-Mega-Evolutions.html";
-         document.getElementById("WNimg").src = "images/megaEvolutionsSmall.png";
-         document.getElementById("WNp").innerHTML = "Jigsaw Puzzles: <br>Pokemon 20th - Mythicals and Mega Evolutions<br><br>3 cool jigsaw puzzles to celebrate the 20th anniversary of the Pokemon series.";
-    }
-    if (time == 5) {
-         document.getElementById("WNdate").innerHTML = "11/27/2015";
-         document.getElementById("WNa").href = "Stone-Kirby-Transformations-Easter-Eggs.html";
-         document.getElementById("WNimg").src = "images/stoneKirbyDreamCollection.png";
-         document.getElementById("WNp").innerHTML = "Stone Kirby Transformations Easter Eggs<br><br> Displaying all of the cool easter eggs related to stone Kirby's transformations.";
-    }*/
 }
 
