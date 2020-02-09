c830dca9d7ae67396adca8b6f25477d716892904
Xander <xanderfix@gmail.com>
2020-02-08 19:49:34 -0700

    Updated Homepage.

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ var time = 1;
 function change() {
     setInterval(function change1() {
     if (time == 0) {
-		 document.getElementById("WNdate").innerHTML = "9/18/2017";
+		 document.getElementById("WNdate").innerHTML = "2/08/2020";
+         document.getElementById("WNa").href = "Flame-Mammoth-Music.html";
+         document.getElementById("WNimg").src = "images/Thumbnail - Flame Mammoth Stage.png";
+         document.getElementById("WNp").innerHTML = "Mega Man X - Flame Mammoth)";
+    }
+    if (time == 1) {
+         document.getElementById("WNdate").innerHTML = "9/18/2017";
          document.getElementById("WNa").href = "The-Super-Ninhundred.html";
          document.getElementById("WNimg").src = "images/SNESgames.jpg";
          document.getElementById("WNp").innerHTML = "The Super Ninhundred<br><br>One man's quest to beat 100 of the best SNES games, 100%. AKA Playing With Super Power!";
     }
-    if (time == 1) {
+    if (time == 2) {
          document.getElementById("WNdate").innerHTML = "6/1/2017";
          document.getElementById("WNa").href = "Zelda-II-Boss-Battle-Cover.html";
          document.getElementById("WNimg").src = "images/ZeldaIIBossBattleThumbnail.png";
          document.getElementById("WNp").innerHTML = "Zelda II - Boss Battle Cover<br><br>Xander's synth cover of Boss Battle from Zelda II: The Adventure of Link for the new Zelda tribute album Hylian Downfall.";
-    }
-    if (time == 2) {
-         document.getElementById("WNdate").innerHTML = "12/24/2016";
-         document.getElementById("WNa").href = "How-to-Play-Jangle-Bells.html";
-         document.getElementById("WNimg").src = "images/DKC3merry.png";
-         document.getElementById("WNp").innerHTML = "How to Play Jangle Bells<br>(DKC3 MERRY Cheat)";
     }     
     if (time == 3) {
          document.getElementById("WNdate").innerHTML = "7/27/2016";
