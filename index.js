c20e83444b945837406c68fab55dd28eb5a11ecf
Xander <xanderfix@gmail.com>
2020-03-22 11:50:42 -0600

    More Mega Man X Tunes

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var time = 1;
 function change() {
     setInterval(function change1() {
     if (time == 0) {
-		 document.getElementById("WNdate").innerHTML = "3/15/2020";
-         document.getElementById("WNa").href = "National-Park-Music.html";
-         document.getElementById("WNimg").src = "images/Thumbnail - National Park.png";
-         document.getElementById("WNp").innerHTML = "Pokémon HG/SS - National Park";
+		 document.getElementById("WNdate").innerHTML = "3/22/2020";
+         document.getElementById("WNa").href = "Armored-Armadillo-Music.html";
+         document.getElementById("WNimg").src = "images/Thumbnail - Armored Armadillo.png";
+         document.getElementById("WNp").innerHTML = "Mega Man X - Armored Armadillo";
     }
     if (time == 1) {
          document.getElementById("WNdate").innerHTML = "9/18/2017";
